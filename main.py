5f9aefbc638467cdc3873720f0d2db9a59f54edb
vlntnk <qwjwxc@gmail.com>
2023-12-18 17:27:57 +0300

    Added gunicorn

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 login_manager = LoginManager(app)
 
 
